da938d540d4ccba151388604b0f7b6de42dcf2fb
myossi <wldbseja@daum.net>
2023-03-30 01:31:10 +0900

    [level 0] Title: 모스부호 (1), Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,17 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+    }
+    
+    const morseArr = letter.split(" ")
+    
+    let str = ''
+    for(let i = 0; i < morseArr.length; i++){
+        str += morse[morseArr[i]]
+    }
+    return str
+}
