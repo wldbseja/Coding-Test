50877a0830a7df4cb3ee01c0efb8d967698f0a7f
YoonE_ <wldbseja@daum.net>
2023-05-03 03:15:29 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,28 +1,29 @@
-function solution(weights){
-    let obj = {};
-    let count = 0;
-    for (const num of weights){
-        if(obj[num] === undefined){
-            obj[num] = 1; 
-        } else {
-            count+=obj[num];
-            obj[num] += 1;
-        };
-    };
+function solution(weights) {
+  let count = 0;
+  const point = [2, 3, 4];
+  const map = new Map();
+  const temp = new Map();
 
-    const arr = Object.keys(obj);
-    const result = {};
+  weights.forEach((v) => {
+    if (!map.has(v)) {
+      map.set(v, 1);
+    } else {
+      count += map.get(v);
+      map.set(v, map.get(v) + 1);
+    }
+  });
 
-    for(let i = 2; i<=4; i++){
-        arr.forEach(el=> {
-            let temp = el * i;
-            if(result[temp] === undefined){
-                result[temp] = obj[el];
-            } else {
-                count += result[temp] * obj[el];
-                result[temp] += obj[el];
-            };
-        })
-    };
-    return count
-};
+  for (const [weight, cnt] of map) {
+    for (const points of point) {
+      const calculate = weight * points;
+      if (!temp.has(calculate)) {
+        temp.set(calculate, cnt);
+      } else {
+        count += temp.get(calculate) * cnt;
+        temp.set(calculate, temp.get(calculate) + cnt);
+      }
+    }
+  }
+
+  return count;
+}
