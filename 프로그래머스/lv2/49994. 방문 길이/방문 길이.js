1e4a3e1339cef4eca6cbebfb539d6037b10a04ee
YoonE_ <wldbseja@daum.net>
2023-06-28 21:00:32 +0900

    [level 2] Title: 방문 길이, Time: 0.61 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -6,27 +6,29 @@ function solution(dirs) {
     D: [0, 1],
     L: [-1, 0]
   };
-  const visited = new Set(); // 방문한 좌표를 저장하는 Set
+  const visited = Array.from(Array(11), () => Array(11).fill(false));
   let answer = 0;
-  let x = 0; // 시작 위치 x 좌표
-  let y = 0; // 시작 위치 y 좌표
+  let x = 5; // 시작 위치 x 좌표
+  let y = 5; // 시작 위치 y 좌표
 
   for (let i = 0; i < dirs.length; i++) {
     const [dx, dy] = dic[dirs[i]];
     const nx = x + dx;
     const ny = y + dy;
-    const key1 = `${x},${y},${nx},${ny}`; // 현재 위치에서 다음 위치로의 키
-    const key2 = `${nx},${ny},${x},${y}`; // 다음 위치에서 현재 위치로의 키
 
-    if (nx < -5 || nx > 5 || ny < -5 || ny > 5) {
+    if (nx < 0 || nx > 10 || ny < 0 || ny > 10) {
       // 범위를 벗어나는 경우, 무시하고 다음 명령어로 넘어감
       continue;
     }
 
-    if (!visited.has(key1) && !visited.has(key2)) {
+    if (!visited[nx][ny]) {
       // 처음 방문하는 경로라면
-      visited.add(key1);
-      visited.add(key2);
+      visited[nx][ny] = true;
+      visited[x][y] = true; // 현재 위치에서 다음 위치로의 방문 체크
+      answer++;
+    } else if (!visited[x][y]) {
+      // 이미 방문한 경로라면, 현재 위치에서 다음 위치로의 방문 체크
+      visited[x][y] = true;
       answer++;
     }
 
