b80c1aaf9e7124fb531cc1aa76d39e7b656d9be7
YoonE_ <wldbseja@daum.net>
2023-06-28 20:59:41 +0900

    [level 2] Title: 방문 길이, Time: 0.61 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -1,34 +1,32 @@
 function solution(dirs) {
   dirs = dirs.split('');
-  const dx = [-1, 0, 1, 0];
-  const dy = [0, 1, 0, -1];
   const dic = {
-    U: 0,
-    R: 1,
-    D: 2,
-    L: 3
+    U: [0, -1],
+    R: [1, 0],
+    D: [0, 1],
+    L: [-1, 0]
   };
-  const map = Array.from(Array(11), () =>
-  Array(11).fill(0).map(() => Array(4).fill(false))
-  );
+  const visited = new Set(); // 방문한 좌표를 저장하는 Set
   let answer = 0;
-  let x = 5; // 시작 위치 x 좌표
-  let y = 5; // 시작 위치 y 좌표
+  let x = 0; // 시작 위치 x 좌표
+  let y = 0; // 시작 위치 y 좌표
 
   for (let i = 0; i < dirs.length; i++) {
-    const direction = dic[dirs[i]];
-    const nx = x + dx[direction];
-    const ny = y + dy[direction];
+    const [dx, dy] = dic[dirs[i]];
+    const nx = x + dx;
+    const ny = y + dy;
+    const key1 = `${x},${y},${nx},${ny}`; // 현재 위치에서 다음 위치로의 키
+    const key2 = `${nx},${ny},${x},${y}`; // 다음 위치에서 현재 위치로의 키
 
-    if (nx < 0 || nx > 10 || ny < 0 || ny > 10) {
+    if (nx < -5 || nx > 5 || ny < -5 || ny > 5) {
       // 범위를 벗어나는 경우, 무시하고 다음 명령어로 넘어감
       continue;
     }
 
-    if (!map[x][y][direction]) {
+    if (!visited.has(key1) && !visited.has(key2)) {
       // 처음 방문하는 경로라면
-      map[x][y][direction] = true;
-      map[nx][ny][(direction + 2) % 4] = true;
+      visited.add(key1);
+      visited.add(key2);
       answer++;
     }
 
