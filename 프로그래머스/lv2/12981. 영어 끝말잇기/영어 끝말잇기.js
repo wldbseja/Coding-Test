b4e6a4d79330177a1012dc225ec77d0dc2e7039f
YoonE_ <wldbseja@daum.net>
2023-08-09 10:27:45 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,19 +1,31 @@
 function solution(n, words) {
-    let usedWords = new Set();
+    let usedWords = [];  // 사용된 단어를 저장할 배열
     
-    usedWords.add(words[0]);
-    for (let i = 1; i < words.length; i++) {
-        // 조건 2: 마지막 글자와 첫 글자가 같아야 한다.
-        if (words[i - 1].charAt(words[i - 1].length - 1) !== words[i].charAt(0)) {
-            return [(i % n) + 1, Math.floor(i / n) + 1];
+    let currentPlayer = 1;  // 현재 단어를 말할 차례의 사용자 번호
+    let turn = 1;  // 사용자의 몇 번째 차례인지 저장
+
+    for (let i = 0; i < words.length; i++) {
+        // 이전 단어와 현재 단어의 끝말잇기 조건을 확인
+        if (i > 0 && words[i - 1][words[i - 1].length - 1] !== words[i][0]) {
+            return [currentPlayer, turn];
         }
-        // 조건 3: 이전에 사용된 단어는 사용할 수 없다.
-        if (usedWords.has(words[i])) {
-            return [(i % n) + 1, Math.floor(i / n) + 1];
+
+        // 사용된 단어인지 확인
+        if (usedWords.includes(words[i])) {
+            return [currentPlayer, turn];
         }
-        usedWords.add(words[i]);
+
+        // 현재 단어를 사용된 단어 리스트에 추가
+        usedWords.push(words[i]);
+
+        // 다음 사용자로 넘어가기
+        if (currentPlayer === n) {
+            currentPlayer = 1;
+            turn++;
+        } else {
+            currentPlayer++;
+        }
     }
-    
-    // 문제 없이 끝난 경우
-    return [0, 0];
+
+    return [0, 0];  // 문제 없이 게임을 마친 경우
 }
